06a88027ceb7d735504932e0501d110c5198c736
remuslum <57741494+remuslum@users.noreply.github.com>
2025-01-15 13:13:58 +0800

    Added nonNull

--- Day25/vttp_5a_paf_day25_lecture_consumer/src/main/java/sg/nus/edu/iss/vttp_5a_paf_day25_lecture_consumer/config/RedisConfig.java
+++ Day25/vttp_5a_paf_day25_lecture_consumer/src/main/java/sg/nus/edu/iss/vttp_5a_paf_day25_lecture_consumer/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
--- Day25/vttp_5a_paf_day25_lecture_consumer/src/main/java/sg/nus/edu/iss/vttp_5a_paf_day25_lecture_consumer/service/OrderService.java
+++ Day25/vttp_5a_paf_day25_lecture_consumer/src/main/java/sg/nus/edu/iss/vttp_5a_paf_day25_lecture_consumer/service/OrderService.java
@@ -2,6 +2,7 @@ package sg.nus.edu.iss.vttp_5a_paf_day25_lecture_consumer.service;
 
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +11,11 @@ public class OrderService implements MessageListener{
     
 
     @Override
-    public void onMessage(Message message, @Nullable byte[] pattern) {
+    public void onMessage(@NonNull Message message, @Nullable byte[] pattern) {
         try {
 
             String orderData = new String(message.getBody());
             System.out.println(orderData);
-
-
-            // using Json-P to map it back to object
-            // call the API in day 24 using RestTemplate to write to MySQL database
 
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
